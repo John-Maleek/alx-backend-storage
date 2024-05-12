4049f191d2dad597c851061bc347dca69c951b0d
John Maleek <jmzoran01@gmail.com>
2024-05-12 01:46:34 +0100

    advanced tasks

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,14 @@
+-- a SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUsers that computes
+-- and store the average weighted score for all students
+
+DROP procedure IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser
+BEGIN
+    UPDATE users
+   	SET average_score=(SELECT AVG(score) FROM corrections
+			     WHERE corrections.user_id=user_id)
+	WHERE id=user_id;
+END;$$
+DELIMITER ;
